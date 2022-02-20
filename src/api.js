4fb056f6c33fee23cd4946541109184caaf9e033
Lucas Ketelhut <lucas-kmk@hotmail.com>
2022-02-20 16:15:23 -0300

    initializing application

---
+++ src/api.js
@@ -0,0 +1,7 @@
+const axios = require("axios")
+const baseURL = "https://api.github.com"
+const gitAPI = axios.create({
+    baseURL
+})
+
+module.exports = gitAPI;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+const cors = require('cors');
+const express = require('express'); // Ferramenta de rotas, retornos, middlewares.. etc
+const routes = require('./routes.js'); // Rota(s) da aplicação
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333, () => {
+    console.log('Server is running!')
+})
